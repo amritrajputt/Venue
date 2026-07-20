21b4211dfb93c5ae0b599540153246f9cbf3cf70
amrit <vishal179singh@gmail.com>
2026-07-20 21:16:54 +0530

    fixed inngest error

--- app/api/inngest/client.ts
+++
@@ -1,7 +0,0 @@
-import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { registrationConfirmationEmail } from "@/inngest/function";
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [registrationConfirmationEmail],
-});
